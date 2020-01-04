5919ec4808ff430f65275ee3b12ee01519a25e3f
Vee <vharbison1@gmail.com>
2020-01-04 14:13:00 -0500

    edited port

--- server.js
+++ server.js
@@ -367,6 +367,6 @@ app.post('/api/login', function (req, res) {
     });
   })
     
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log('Todo List API is now listening on port 3000...');
 })
